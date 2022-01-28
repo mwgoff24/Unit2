4a1776936040f1f1fe0f3a077f1d97a166dd76cc
Martin <mwgoff24@students.bmchs.org>
2022-01-28 09:46:26 -0600

    started work

---
+++ Lab_2.03.py
@@ -0,0 +1,117 @@
+'''
+##########################################
+Lab 2.03 - Game Show
+##########################################
+In your Notebook
+Follow the flow of execution in the following programs and predict what will happen for each
+one
+---------------------------------------
+Example 1
+---------------------------------------
+a = input("What... is your quest")
+b = "to seek the holy grail"
+if a != b:
+    print("Go On. Off you go")
+else:
+    b = input("What...is the air-speed velocity of an unladen swallow?")
+    if b == "What do you mean? An African or European swallow?":
+        print("I don't know that...AHHH [Bridgekeeper was thrown over bridge]")
+    else:
+        print("[you were thrown over bridge]")
+
+Prediction: 
+The program asks for one's quest. If their input is to seek the holy grail, the program will say to go on.
+If they are not seeking the grail, they are asked for the air-speed velocity of an unladen swallow.
+If they ask to be more specific, the bridgekeeper won't know. Else, the program says the person was thrown over.
+
+Actual:
+Mostly the same, but the program jumps to the question on swallows if you are seeking the holy grail, and any other answer lets you through.
+
+---------------------------------------
+Example 2
+---------------------------------------
+user_input = input("What is your favorite color")
+if user_input == 'blue':
+    print("Blueskadoo")
+elif user_input == "red":
+    print("Roses are red!")
+elif user_input == "yellow":
+    print("Mellow Yellow")
+elif user_input == "green":
+    print("Green Machine")
+elif user_input == "orange":
+    print("Orange you glad I didn't say banana.")
+elif user_input == "black":
+    print("I see a red door and I want it painted black")
+elif user_input == "purple":
+    print("And we'll never be royalllssss")
+elif user_input == "pink":
+    print("Pinky- and the Brain")
+else:
+    print("I don't recognize that color. Is it even...??")
+
+Prediction: 
+Whatever color is picked will send back a message related to that color. 
+If that color isn't listed, the program returns it doesn't recognize that color.
+
+Actual: 
+Same as prediction.
+
+---------------------------------------
+In your Notebook
+---------------------------------------
+Translate this Snap! program into a Python program
+***Refer to the image provided on Moodle located below the Lab 2.03 link***
+Write program below:
+
+---------------------------------------
+Create a game show program
+---------------------------------------
+Declare 10 prizes (prize1, prize2, prize 3 at the top of your file)
+User picks a number
+The prize corresponding with that door is printed for the user.
+Write code below the multiline comment
+'''
+#Monty Python Code / Example 1
+'''
+a = input("What... is your quest")
+b = "to seek the holy grail"
+if a != b:
+    print("Go On. Off you go")
+else:
+    b = input("What...is the air-speed velocity of an unladen swallow?")
+    if b == "What do you mean? An African or European swallow?":
+        print("I don't know that...AHHH [Bridgekeeper was thrown over bridge]")
+    else:
+        print("[you were thrown over bridge]")
+'''
+#Colors Code / Example 2
+'''
+user_input = input("What is your favorite color")
+if user_input == 'blue':
+    print("Blueskadoo")
+elif user_input == "red":
+    print("Roses are red!")
+elif user_input == "yellow":
+    print("Mellow Yellow")
+elif user_input == "green":
+    print("Green Machine")
+elif user_input == "orange":
+    print("Orange you glad I didn't say banana.")
+elif user_input == "black":
+    print("I see a red door and I want it painted black")
+elif user_input == "purple":
+    print("And we'll never be royalllssss")
+elif user_input == "pink":
+    print("Pinky- and the Brain")
+else:
+    print("I don't recognize that color. Is it even...??")
+'''
+#Snap! Recreation / Example 3
+side1=input("What is the length of your first side? ")
+side2=input("What is the length of your second side? ")
+side3=input("What is the length of your second side? ")
+if int(side1)+int(side2)>int(side3) and int(side1)+int(side3)>int(side2) and int(side2)+int(side3)>int(side1):
+    print(f"The perimeter is {int(side1)+int(side2)+int(side3)}.")
+else:
+    print("Those inputs don't make a triangle.")
