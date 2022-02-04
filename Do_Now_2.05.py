36b9d551738f5a4168b8d10e6823bdcac844d68b
Martin Goff <martingoff@Goffs-MacBook-Pro.local>
2022-02-04 14:19:28 -0600

    did do now 2.04

---
+++ Do_Now_2.05.py
@@ -0,0 +1,60 @@
+'''#####################
+Do Now 2.05
+#####################
+Part 1 - Open up the console
+----------------------------
+Type the following example code using interactive mode
+Example 1
+ >>> a = [123, 'First Item', 456, 'Second Item']
+ >>> b = a[0:2]
+ >>> print(a)
+ >>> print(b)
+In your notebook
+Respond to the following
+What happens to a? Stays the same.
+
+What is b? Displays items 0 and 2 from a.
+
+What type is b? List.
+
+Example 2
+ >>> a = [123, 'First Item', 456]
+ >>> a.remove('First Item')
+ >>> print(a)
+Continue in your notebook
+Respond to the following questions
+What does remove do? Removes an item from a list.
+
+What would the length of this list be after remove? 2
+
+Example 3
+ >>> a = [123, 'First Item', 456]
+ >>> a.pop()
+ >>> print(a)
+Continue your responses in your notebook
+What does pop do? Removes an index and returns it.
+
+What is the difference between remove and pop? Pop also prints what is removed.
+
+Example 4
+ >>> a = [123, 'First Item', 456, 'Second Item']
+ >>> b = a + ['Third Item']
+ >>> print(a)
+ >>> print(b)
+Continue your responses to the following in your notebook
+What happens to a? Stays the same.
+
+What is b? List a with an added item.
+
+What type is b? List.
+
+Example 5
+ >>> a = []
+ >>> print(len(a))
+ >>> a.append('First Item')
+ >>> print(a)
+In your Notebook
+What was the length of []? 0.
+
+What does append do? What would the length be after append? Adds an item to the end of a list. 1.
+'''
