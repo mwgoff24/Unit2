d0aa7e1e455099c7e87b6f8d8ae9bfd02a39cdbe
Martin <mwgoff24@students.bmchs.org>
2022-02-08 09:19:19 -0600

    worked more

--- Lab_2.06.py
+++ Lab_2.06.py
@@ -54,3 +54,29 @@ Print the updated board out.
 You will not need to determine the winner at this point.
 (Copy and paste your previous tic-tac-toe version and modify the code to implement the above)
 '''
+#defining the spots on the table
+board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+#tic tac toe table
+print(f"{board[0]} | {board[1]} | {board[2]} \n"
+"--------- \n"
+f"{board[3]} | {board[4]} | {board[5]} \n"
+"--------- \n"
+f"{board[6]} | {board[7]} | {board[8]}")
+
+turn_number = 1
+
+#replacing spot with X
+while turn_number <=9:
+    turn = int(input("Pick a spot to place your marker on. "))-1
+    if turn >= 10 or turn <= -1:
+        print("That's not a spot.")
+    else:
+        board[turn] = 'X'
+        print(f"{board[0]} | {board[1]} | {board[2]} \n"
+        "--------- \n"
+        f"{board[3]} | {board[4]} | {board[5]} \n"
+        "--------- \n"
+        f"{board[6]} | {board[7]} | {board[8]}")
+        turn_number = turn_number+1
+
