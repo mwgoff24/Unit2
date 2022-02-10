65374b2bfd56f40c0944bd96c6c8c228b37e2d62
Martin <mwgoff24@students.bmchs.org>
2022-02-10 09:49:16 -0600

    started project

---
+++ project2.py
@@ -0,0 +1,6 @@
+#Text Monster Game
+
+#Map of dungeon
+floor3 = ['prize', 'boss monster', 'down-stairs', 'sword', 'magic stones']
+floor2 = []
+floor1 = ['nothing', 'sword', 'up-stairs', 'monster', 'magic stones']
